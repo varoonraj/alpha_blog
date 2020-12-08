ca2894b89eee813c69c132f75b58226971a46d8a
Varun Raj <leo.varun87@gmail.com>
2020-12-08 14:36:24 +0530

    alpha_complete

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-  validates :title, presence: true, length: {minimum: 6, maximum:100}
-  validates :description, presence: true, length: {minimum: 100, maximum: 400}
+  validates :title, presence: true, length: {minimum: 4, maximum:100}
+  validates :description, presence: true, length: {minimum: 6, maximum: 400}
 
 end
