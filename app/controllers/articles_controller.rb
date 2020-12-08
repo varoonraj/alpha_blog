050d1e6960fde6a38086695f8c51d1b5882a2e26
Varun Raj <leo.varun87@gmail.com>
2020-12-08 14:53:16 +0530

    small correction in edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,7 +28,7 @@ class ArticlesController < ApplicationController
   def update
     if @article.update(article_params)
       flash[:notice] ="Article updated"
-
+      redirect_to @article
     else
       render 'edit'
     end
